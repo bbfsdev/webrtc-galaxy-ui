5b9f536fb4b93d4bcacdb9f434f870f5e05ed95d
guya <guy.atia@gmail.com>
2014-07-23 06:05:26 +0300

    remove group in preset / remove preset with ctrl

--- js/controllers.js
+++ js/controllers.js
@@ -1,13 +1,30 @@
 function onLoadCtrl ($scope, $rootScope, $translate) {
-	 $scope.keyDown = function(eve){
-       if(eve.which === 17){ //ctrl
-           $rootScope.ctrlDown = true;   
-       }
+	 $scope.keyDown = function(event){
+      if(event.which === 17){ //ctrl
+         $rootScope.ctrlDown = true;   
+      }
+      if(event.which === 18){ //alt
+         $rootScope.altDown = true;   
+      }
+      
+      if(event.which === 49) //1
+      {
+        alert('1');
+      } 
+      if(event.which === 50) //2
+      {
+        alert('2');
+      } 
+       
     }
-    $scope.keyUp = function(eve){
-       if(eve.which === 17){ //ctrl
+    $scope.keyUp = function(event){
+       if(event.which === 17){ //ctrl
            $rootScope.ctrlDown = false;
        }
+       if(event.which === 18){ //alt
+           $rootScope.altDown = false;   
+       }
+
     }
 }
 onLoadCtrl.$inject = ["$scope", "$rootScope", "$translate"];
@@ -36,15 +53,34 @@ previewCtrl.$inject = ["$scope","$rootScope"];
 
 function presetsCtrl ($scope,$rootScope) {
     $scope.presets = [];
+    $scope.presetIndex = 0;
+
     $scope.addPreset = function() {
         $scope.presets.push([]);
         $scope.presetIndex = $scope.presets.length-1;
     }
-    $scope.presetIndex = 0;
-  	$scope.presetClicked = function ($index) {
-    	$scope.presetIndex = $index;
+    
+  	$scope.presetClicked = function (index) {
+      if ($rootScope.ctrlDown)
+      {
+        if ($scope.presets[index].length == 0)
+          $scope.presets.splice(index, 1);
+        return;
+      }
+    	$scope.presetIndex = index;
       $rootScope.$broadcast('showPreview', $scope.selectedPreset());
   	}
+
+    $scope.removeGroup = function (preset, group) {
+      if (!$rootScope.ctrlDown)
+        return;   
+      if (preset != null) {
+        var curGroupIndex = preset.indexOf(group);
+        if (curGroupIndex != -1)
+          preset.splice(curGroupIndex, 1);
+      }
+    }
+    
     $scope.$on("addGroupToPreset", function (e, group) {
         if ($scope.presets.length == 0)
             $scope.addPreset();
@@ -54,8 +90,8 @@ function presetsCtrl ($scope,$rootScope) {
             var curGroupIndex = curPreset.indexOf(group);
             if (curGroupIndex == -1)
                 curPreset.push(group);
-            else
-                curPreset.splice(curGroupIndex, 1);
+            /*else
+                curPreset.splice(curGroupIndex, 1);*/
         }
     });
     $scope.selectedPreset = function()
